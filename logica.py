3c597d95b6bc66180fbb8634ae2791ea6acbfc61
Anita Palhares <anitapalharesb@gmail.com>
2026-04-20 17:58:30 -0300

    Add files via upload

---
+++ logica.py
@@ -0,0 +1,97 @@
+import random 
+
+user_email = str(input("Digite o seu Email: "))
+user_password = int(input("Digite a sua Senha Numérica: "))
+
+list_user_input_email = [
+    "kaua@gmail.com",
+    "vitoriagmail.com",
+    "anita@gmailcom",
+    "  MARIA@GMAIL.COM  ",
+    "lucas@g mail com",
+]
+
+list_user_input_email.append(user_email)
+
+list_user_valid_email = []
+list_user_invalid_email = []
+
+for email in list_user_input_email:
+    valid_email = email.strip().lower().replace(" ", "")
+    if "@" not in valid_email or "." not in valid_email or len(valid_email) > 25:
+        list_user_invalid_email.append(email)
+        is_email_valid = False
+    else:
+        list_user_valid_email.append(valid_email)
+        is_email_valid = True
+
+list_user_input_password = [
+    1234,
+    5678,
+    123,
+    123456789
+]
+
+list_user_input_password.append(user_password)
+
+list_user_valid_password = []
+list_user_invalid_password = []
+
+for password in list_user_input_password:
+    if len(str(password)) > 3 and len(str(password)) < 9:
+        list_user_valid_password.append(password)
+        is_password_valid = True
+    else:
+        list_user_invalid_password.append(password)
+        is_password_valid = False
+
+print(f"-" * 5, "EMAILS E SENHAS CORRETOS", "-" * 5)
+for i in list_user_valid_email:
+    print(i)
+for i in list_user_valid_password:
+    print(i)
+
+print(f"-" * 5, "EMAILS E SENHAS INCORRETOS", "-" * 5)
+for i in list_user_invalid_email:
+    print(i)
+for i in list_user_invalid_password:
+    print(i)
+
+if is_email_valid == True and is_password_valid == True:
+    print("="*30)
+    print(f"-" * 5, "INICIANDO O JOGO", "-" * 5,)
+    print("="*30)
+    
+    nomes_proibidos = ["admin", "deus", "mestre"]
+
+while True:
+    character_user = input("Digite o nome do seu personagem: ")
+    nome = character_user.strip().replace(" ", "_").isalpha()
+    if nome in nomes_proibidos:
+        print("Esse nome não é permitido! Escolha outro.")
+        continue
+    print(f"O nome {character_user} é válido!\n CONTINUE E ESCOLHA SEU TIPO DE PERSONAGEM!\n")
+    itens = []
+    number_choice = input('DIGITE O NÚMERO:\n1 - Guerreiro (tanque)\n2 - Assasino (DPS)\n3 - Mago (conjurador)\n4 - Clérico (curandeiro)\n5 - Arqueiro (suporte)')
+    match number_choice:
+        case 1:
+            print('Você escolheu ser um Guerreiro')
+            itens.extend(["Martelo","Canhão","Espada"])
+            type_character = "guerreiro"
+        case 2:
+            print('Você escolheu ser um Assasino')
+            itens.extend(["Faca Dupla","Veneno","Corda"])
+            type_character = "Assasino"
+        case 3:
+            print('Você escolheu ser um Mago')
+            itens.extend(["Vara","Kit Poçoes","Barsa"])
+            type_character = "Mago"
+        case 4:
+            print('Você escolheu ser um Clérico')
+            itens.extend(["Ervas","Biblia","Cruz Grande"])
+            type_character = "Clérico"
+        case 5:
+            print('Você escolheu ser um Arqueiro')
+            itens.extend(["Arco e flecha","Querosene","Granadas"])
+            type_character = "Arqueiro"
+    break   
